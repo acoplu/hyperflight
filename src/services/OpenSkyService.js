21c19cc3817a3a09ea5ffc9a6c2400388f251a7e
acoplu <acoplu@etu.edu.tr>
2024-03-19 01:33:38 +0300

    Update OpenSkyService using new defined type

--- src/services/OpenSkyService.js
+++ src/services/OpenSkyService.js
@@ -1,10 +1,13 @@
 // src/services/OpenSkyService.js
+const fetch = require("node-fetch");
+const FlightState = require('../models/FlightState');
+
 const API_URL = 'https://opensky-network.org/api';
 
 async function getAllFlights(boundingBox) {
     try {
         let url = `${API_URL}/states/all?`
-        
+
         if (boundingBox) {
             url += `lamin=${boundingBox.lamin}&lomin=${boundingBox.lomin}&lamax=${boundingBox.lamax}&lomax=${boundingBox.lomax}`;
         }
@@ -15,7 +18,9 @@ async function getAllFlights(boundingBox) {
             console.error('URL: ', url)
             throw new Error('Network response was not ok');
         }
-        return await response.json();
+
+        const data = await response.json();
+        return data.states.map(state => new FlightState(...state));
     } catch (error) {
         console.error('Error fetching flight data:', error);
         return [];
