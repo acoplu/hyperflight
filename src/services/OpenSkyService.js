134f7704fa11b6827113bcee249f1c8dbe772bb1
acoplu <acoplu@etu.edu.tr>
2024-03-19 02:22:17 +0300

    Add some error checks to getAllFlights function

--- src/services/OpenSkyService.js
+++ src/services/OpenSkyService.js
@@ -6,25 +6,47 @@ const API_URL = 'https://opensky-network.org/api';
 
 async function getAllFlights(boundingBox) {
     try {
-        let url = `${API_URL}/states/all?`
+        let url = `${API_URL}/states/all?`;
 
         if (boundingBox) {
-            url += `lamin=${boundingBox.lamin}&lomin=${boundingBox.lomin}&lamax=${boundingBox.lamax}&lomax=${boundingBox.lomax}`;
+            if (isValidBoundingBox(boundingBox)) {
+                url += `lamin=${boundingBox.lamin}&lomin=${boundingBox.lomin}&lamax=${boundingBox.lamax}&lomax=${boundingBox.lomax}`;
+            } else {
+                throw new Error('Invalid bounding box parameters');
+            }
         }
 
         const response = await fetch(url);
         if (!response.ok) {
-            console.error('Response: ', response)
-            console.error('URL: ', url)
-            throw new Error('Network response was not ok');
+            console.error('Response status:', response.status);
+            console.error('Response status text:', response.statusText);
+            console.error('URL:', url);
+            if (response.status === 404) {
+                throw new Error('Flight data not found');
+            } else {
+                throw new Error('Error fetching flight data');
+            }
         }
 
         const data = await response.json();
-        return data.states.map(state => new FlightState(...state));
+        return { time: data.time, states: data.states.map(state => new FlightState(...state)) }; // Include both time and states in the returned object
     } catch (error) {
         console.error('Error fetching flight data:', error);
-        return [];
+        return { time: null, states: [] }; // Return default values if an error occurs
     }
+}
+
+function isValidBoundingBox(boundingBox) {
+    if (!boundingBox) return false;
+
+    // Check if all required properties are present and valid
+    if (!('lamin' in boundingBox && 'lomin' in boundingBox && 'lamax' in boundingBox && 'lomax' in boundingBox)) {
+        return false;
+    } else if (typeof boundingBox.lamin !== 'number' || typeof boundingBox.lomin !== 'number' || typeof boundingBox.lamax !== 'number' || typeof boundingBox.lomax !== 'number') {
+        return false;
+    } else if (boundingBox.lamin >= boundingBox.lamax || boundingBox.lomin >= boundingBox.lomax) {
+        return false;
+    } else return true;
 }
 
 module.exports = getAllFlights;
