d9c12db61436a063e81b310114925c6f4d343975
acoplu <acoplu@etu.edu.tr>
2024-03-19 12:32:20 +0300

    Make map default OpenLayers map

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import './styles.css';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
 import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
@@ -6,8 +9,11 @@ import View from 'ol/View';
 import Link from 'ol/interaction/Link';
 
 const map = new Map({
-    target: 'map-container',
+    target: 'map',
     layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
         new VectorLayer({
             source: new VectorSource({
                 format: new GeoJSON(),
