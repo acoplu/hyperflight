72cc96ae272ccbf9def1489e3998e9e6b185597e
hasanuzunx <hasantalha135@gmail.com>
2024-03-19 11:22:50 +0300

    Change map API google map to openlayers and some file updates

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
-// src/components/MapComponent.js
-const getAllFlights = require('../services/OpenSkyService.js');
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import Link from 'ol/interaction/Link';
 
-function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: {lat: 39, lng: 32} // Default center (Ankara)
-    });
+const map = new Map({
+    target: 'map-container',
+    layers: [
+        new VectorLayer({
+            source: new VectorSource({
+                format: new GeoJSON(),
+                url: './data/countries.json',
+            }),
+        }),
+    ],
+    view: new View({
+        center: [0, 0],
+        zoom: 2,
+    }),
+});
 
-    // Fetch real flight data
-    getAllFlights()
-        .then(flightData => {
-            // Create markers for each flight
-            flightData.states.forEach(flight => {
-                // Create a marker for the plane's position
-                const planeMarker = new google.maps.Marker({
-                    position: { lat: flight.latitude, lng: flight.longitude },
-                    map: map,
-                    icon: {
-                        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, // Use a forward arrow as the icon
-                        scale: 5,                                          // Set the size of the arrow
-                        fillColor: "#FF0000",                              // Set the color of the arrow
-                        fillOpacity: 1,                                    // Set the opacity of the arrow
-                        strokeWeight: 1,                                   // Set the weight of the arrow's outline
-                        rotation: flight.trueTrack,                        // Set the rotation angle of the arrow (in degrees)
-                    },
-                    title: flight.callsign,                                // Tooltip text for the marker
-                });
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching flight data:', error);
-        });
-}
+map.addInteraction(new Link());
