6b86cb464718105185e1f570f05e9bc255cc2a6b
hasanuzunx <hasantalha135@gmail.com>
2024-04-06 23:58:54 +0300

    Icon update for planes in main.js

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import Link from 'ol/interaction/Link';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+
+// Uçak özellikleri için yeni bir stil tanımlayın
+const airplaneStyle = new Style({
+    image: new Icon({
+        anchor: [0.5, 0.5],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        src: './static/plane.png', // Uçak ikonunuzun yolunu buraya girin
+        scale: 1, // Haritanın ölçeğine uygun şekilde ayarlayın
+    }),
+});
 
 const map = new Map({
     target: 'map',
@@ -19,6 +32,7 @@ const map = new Map({
                 format: new GeoJSON(),
                 url: './data/flights.json',
             }),
+            style: airplaneStyle, // Her özelliğe uçak stilini uygula
         }),
     ],
     view: new View({
